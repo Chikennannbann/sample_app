e6475fb427d007c216fa150a3fa6981c6c693cc1
Chikennannbann <kamsk517@gmail.com>
2022-04-28 13:47:43 +0000

    branch 練習1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
    get 'top' => 'homes#top'
   resources :lists
+  patch 'list/:id' => 'lists#update', as: 'update_list'
+#  resouces :lists: only[:update]でパスを作れば追記なしでもいける
+#  updateは追記がいる４行目
 end
